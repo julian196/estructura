05fe28db04570bbaecc8d83e07edd8f9c9d6f445
julian196 <julianvegatorres96@gmail.com>
2019-04-26 20:17:47 -0500

    Create estructura2.php

---
+++ estructura2.php
@@ -0,0 +1,41 @@
+<?php 
+
+	$monto=1000;//indicar si el monto tiene descuento (monto >1000)
+	$edad;//indicar si es mayor de edad o menor de edad
+	$sueldo;//indicar si el sueldo es minimo ono
+	/*
+if (true) {
+	echo "Mensaje por ser true";
+}else
+{
+echo "Mensaje por ser false";
+}
+echo "<br/>";
+
+echo false?"Mensaje por ser true":"Mensaje por ser false";
+echo "<br/>";
+*/
+/*
+	if (monto >1000 {
+		echo "Ay descuento";
+	}else{
+		echo "No ay descuento";
+	}
+*/
+
+if ($monto > 1000) {
+    echo "ay descuento";
+} else {
+    echo "no ay descuento";
+}
+
+if ($sueldo > 930) {
+    echo "tienes un sueldo minimo";
+} else {
+    echo "no tienes un sueldo minimo";
+}
+
+echo edad=>18?"mayor de edad":"menor de edad"
+
+
+ ?>
